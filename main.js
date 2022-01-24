51b891d21ece5dc4773cf23f193f8f6ac88bd69c
Uzlopak <aras.abbasi@googlemail.com>
2022-01-24 04:54:51 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 const mongoose = require('mongoose');
+const ReplSet = require('mongodb-memory-server').MongoMemoryReplSet;
 
-async function main()
-{
-  await mongoose.connect('mongodb://127.0.0.1:27017/mvce');
+async function main() {
+
+    // Create new instance
+    const replSet = new ReplSet({
+        instanceOpts: [
+            // Set the expiry job in MongoDB to run every second
+            {
+                port: 27017,
+                args: ["--setParameter", "ttlMonitorSleepSecs=1"]
+            },
+        ],
+        dbName: 'mongoose_test',
+        replSet: {
+            name: "rs0",
+            count: 2,
+            storageEngine: "wiredTiger",
+        },
+    });
+
+    await replSet.start();
+    await replSet.waitUntilRunning();
+    const uri = replSet.getUri('mvce');
+    await mongoose.connect(uri);
 
 
-  process.exit();
+    process.exit();
 }
 
 main();
