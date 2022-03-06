d3e743be8e2030d149ca104e0aabee5e3a0e89f2
uzlopak <aras.abbasi@googlemail.com>
2022-03-06 22:00:57 +0100

    reproduce gh-11460

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
+const { performance } = require('perf_hooks');
 const ReplSet = require('mongodb-memory-server').MongoMemoryReplSet;
+
+const Person = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
 
 async function main() {
 
@@ -23,7 +29,26 @@ async function main() {
     await replSet.start();
     await replSet.waitUntilRunning();
     const uri = replSet.getUri('mvce');
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        minPoolSize: 100,
+        maxPoolSize: 200
+    });
+
+    const personModel = new mongoose.model('person', Person);
+
+    const personId = (await personModel.collection.insertOne({ firstName: "Eva", lastName: "Goodspeed" })).insertedId;
+
+    for (let i = 0; i < 200; ++i) {
+        let startTime = performance.now();
+        await personModel.findOneAndUpdate({ _id: personId }, { firstName: "bla" + i }).lean();
+        console.log(`Call took ${Math.floor((performance.now() - startTime))} ms`);
+    }
+
+    const personUpdated = await personModel.findOne({_id: personId});
+
+    console.log(personUpdated);
 
 
     process.exit();
